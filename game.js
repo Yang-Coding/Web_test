297a19b550ad23dc1ea6d1fbafaf4dc6cdcd6c13
Yáng <limziyang2004@gmail.com>
2021-06-05 16:07:27 +0800

    Update game.js

--- game.js
+++ game.js
@@ -242,22 +242,20 @@ function spawn() {
 }
 
 // shoot projectile // 
-window.addEventListener('keypress', logKey,(event) => {
+window.addEventListener('keypress', (event) => {
 
-    function logKey(e) {
-        const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
 
-        const velocity = {
-            x: Math.cos(angle) * 5,
-            y: Math.sin(angle) * 5
-        }
-
-        projectiles.push(new Projectile(
-            canvas.width / 2,
-            canvas.height / 2,
-            5,
-            'white', velocity))
+    const velocity = {
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
     }
+
+    projectiles.push(new Projectile(
+        canvas.width / 2,
+        canvas.height / 2,
+        5,
+        'white', velocity))
 })
 
 btn.addEventListener('click', () => {
